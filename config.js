255a08c6ff3ccf55e5b534d09ebd7c2540670cfa
Shiro <ashirosleepy831@gmail.com>
2026-08-28 11:02:21 +0700

    1

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 // vì quyền truy cập dữ liệu đã được kiểm soát bằng RLS trong Supabase.
 // ============================================================
 
-const SUPABASE_URL = "https://mdkdhtnigwkvlavavfck.supabase.co";
+const SUPABASE_URL = "https://kcbhupnwwcnlfzlykbfh.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ka2RodG5pZ3drdmxhdmF2ZmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4NTEwMDAsImV4cCI6MjEwMzQyNzAwMH0._am7is90HeGwp_I-_zVehPMwSC_zDxuMn1DCiudc8y4";
